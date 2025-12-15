ac5ce64d96f1f2f6fb15de58f442e8c85a9becdc
jing388 <uy.gwynethfabul@gmail.com>
2025-12-15 17:48:44 +0800

    removed backend

--- backend/src/routes/documentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateRescueReport } = require("../controllers/documentController");
-
-router.get("/rescueReports/:alertID", generateRescueReport);
-
-module.exports = router;
--- backend/src/routes/graphRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAlertStats } = require("../controllers/graphController");
-
-router.get("/graph", getAlertStats);
-
-module.exports = router;
